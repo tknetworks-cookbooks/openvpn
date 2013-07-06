ec481605a9dcf3dfe324c97add8617b5f257ac4f
TANABE Ken-ichi <nabeken@tknetworks.org>
2013-07-06 22:17:13 +0900

    attributes: cleanup

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,18 +16,14 @@
 case platform
 when "freebsd"
   default['openvpn']['dir'] = "/usr/local/etc/openvpn"
-  default['openvpn']['uid'] = "nobody"
-  default['openvpn']['gid'] = "nogroup"
-  default['openvpn']['service'] = "openvpn"
-  default['openvpn']['package'] = "openvpn"
 else
   default['openvpn']['dir'] = "/etc/openvpn"
-  default['openvpn']['uid'] = "nobody"
-  default['openvpn']['gid'] = "nogroup"
-  default['openvpn']['service'] = "openvpn"
-  default['openvpn']['package'] = "openvpn"
 end
 
+default['openvpn']['service'] = "openvpn"
+default['openvpn']['package'] = "openvpn"
+default['openvpn']['uid'] = "nobody"
+default['openvpn']['gid'] = "nogroup"
 default['openvpn']['ssl']['dh'] = "#{default['openvpn']['dir']}/dh.pem"
 default['openvpn']['ssl']['dh_bit'] = "2048"
 default['openvpn']['max_clients'] = 10
